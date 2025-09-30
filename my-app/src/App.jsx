9cfaa9913ecfbd7825a55b9585b6a26dccb47a98
Ajithkumar <ajithsivu@gmail.com>
2025-09-30 11:59:00 +0530

    Update App.jsx

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,9 +1,7 @@
-
 import React, { useEffect, useState, createContext } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import axios from "axios";
 import UsersList from "./UsersList.jsx";
-
 
 export const UserContext = createContext();
 
@@ -20,22 +18,16 @@ const App = () => {
   return (
     <UserContext.Provider value={users}>
       <Router>
-        <div className="p-6">
-          <h1 className="text-2xl font-bold mb-4">Users</h1>
-          <nav className="flex gap-4 mb-6">
-            <Link to="/names" className="px-4 py-2 bg-blue-500 text-white rounded-lg">
-              Show Names</Link>
-
-            <Link to="/emails" className="px-4 py-2 bg-green-500 text-white rounded-lg">
-              Show Emails
-            </Link>
-
+        <div className="container">
+          <h1>Users</h1>
+          <nav>
+            <Link to="/names" className="names-link">Show Names</Link>
+            <Link to="/emails" className="emails-link">Show Emails</Link>
           </nav>
 
           <Routes>
             <Route path="/names" element={<UsersList field="name" />} />
             <Route path="/emails" element={<UsersList field="email" />} />
-        
           </Routes>
         </div>
       </Router>
